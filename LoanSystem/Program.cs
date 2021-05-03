d4e94f5e8efbbb04f841fcf28998e8d31b9c879f
Kian Magdamit <kcmagdamit@iskolarngbayan.pup.edu.ph>
2021-05-03 14:37:38 +0800

    Revert "3rd commit"
    This reverts commit dd7931818957fd9bf6bfaa445823b5551b3caad0.
    
    3rd commit
    
    3rd commit
    
    2nd commit.

--- LoanSystem/Program.cs
+++ LoanSystem/Program.cs
@@ -5,7 +5,7 @@ namespace LoanSystem
 {
     class Program
     {
-        public static List<string> products = new List<string> { "IPHONE === ₱45,000","SMARTWATCH === ₱15,000","GOPRO === ₱20,000",
+        public static List<string> ProductName = new List<string> { "IPHONE === ₱45,000","SMARTWATCH === ₱15,000","GOPRO === ₱20,000",
                                                                  "TOYOTA WIGO === ₱650,000  ","HONDA CIVIC === ₱700,000","FORD FIESTA === ₱500,000"
                                                                    ,"SMART TV === ₱20,000","AIRCON === ₱10,000","DINING TABLE === ₱5,000"};
 
@@ -14,48 +14,73 @@ namespace LoanSystem
         {
 
             Console.Title = "LoanSystem";
-            Console.WriteLine("-------------------------------");
-            Console.WriteLine("Welcome to Loan System");
-            Console.WriteLine("-------------------------------");
+            Console.WriteLine("===============================");
+            Console.WriteLine("\n Welcome to Loan System \n");
+            Console.WriteLine("===============================");
+            Console.WriteLine("Press [0] to enter your information");
+            Console.WriteLine("LOAN MENU");
+            Console.WriteLine("===============================");
+            Console.WriteLine("\n [1]Gadget Loan     \n");
+            Console.WriteLine("\n [2]Car Loan        \n");
+            Console.WriteLine("\n [3]Appliances Loan \n");
+            Console.WriteLine("===============================");
 
-            Console.WriteLine("Choose your desired loan");
-            Console.WriteLine("***************");
-            Console.WriteLine("[1]Gadget Loan");
-            Console.WriteLine("[2]Car Loan");
-            Console.WriteLine("[3]Appliances Loan");
-            Console.WriteLine("***************");
+            string UserInput = Console.ReadLine();
 
-            string input = Console.ReadLine();
-
-            if (input == "1")
+            if (UserInput == "1")
             {
                 gadgetloan();
             }
-            else if (input == "2")
+            else if (UserInput == "2")
             {
                 carloan();
             }
-            else if (input == "3")
+            else if (UserInput == "3")
             {
                 appliancesloan();
+            }
+            else if (UserInput == "0")
+            {
+                UserInfo();
+            }
+
+
+            static void UserInfo()
+            {
+
+                Console.Clear();
+                
+                Console.WriteLine("Enter your FullName:");
+                var name = Console.ReadLine();
+                
+                Console.WriteLine("Enter your Age:");
+                var age = Console.ReadLine();
+
+                Console.WriteLine("Enter your Occupation:");
+                var occupation = Console.ReadLine();
+
+                Console.WriteLine("\n Here are your details \n");
+                Console.WriteLine($"Name:         {name}        ");
+                Console.WriteLine($"Age:          {age}         ");
+                Console.WriteLine($"Occupation:   {occupation}  ");
             }
 
 
         }
-        static void gadgetloan()
+            static void  gadgetloan()
         {
 
 
             Console.Clear();
-            Console.WriteLine("You choose gadget loan");
+            Console.WriteLine("\n You choose gadget loan \n");
             Console.WriteLine();
 
             Console.WriteLine("Available Gadgets:");
-            Console.WriteLine("**********************");
-            Console.WriteLine(products[0]);
-            Console.WriteLine(products[1]);
-            Console.WriteLine(products[2]);
-            Console.WriteLine("**********************");
+            Console.WriteLine("===============================");
+            Console.WriteLine(ProductName[0]);
+            Console.WriteLine(ProductName[1]);
+            Console.WriteLine(ProductName[2]);
+            Console.WriteLine("===============================");
             Console.WriteLine();
             Console.WriteLine("To Calculate your interest for your loan press ENTER");
             Console.ReadLine();
@@ -88,11 +113,11 @@ namespace LoanSystem
             Console.WriteLine("You choose car loan");
             Console.WriteLine();
             Console.WriteLine("Available Vehicles:");
-            Console.WriteLine("**********************");
-            Console.WriteLine(products[3]);
-            Console.WriteLine(products[4]);
-            Console.WriteLine(products[5]);
-            Console.WriteLine("**********************");
+            Console.WriteLine("===============================");
+            Console.WriteLine(ProductName[3]);
+            Console.WriteLine(ProductName[4]);
+            Console.WriteLine(ProductName[5]);
+            Console.WriteLine("===============================");
             Console.WriteLine("To Calculate your interest for your loan press ENTER:");
             Console.ReadLine();
 
@@ -125,11 +150,11 @@ namespace LoanSystem
             Console.Clear();
             Console.WriteLine("You choose appliances loan");
             Console.WriteLine("Available Appliances:");
-            Console.WriteLine("**********************");
-            Console.WriteLine(products[6]);
-            Console.WriteLine(products[7]);
-            Console.WriteLine(products[8]);
-            Console.WriteLine("**********************");
+            Console.WriteLine("===============================");
+            Console.WriteLine(ProductName[6]);
+            Console.WriteLine(ProductName[7]);
+            Console.WriteLine(ProductName[8]);
+            Console.WriteLine("===============================");
             Console.WriteLine("To Calculate your interest for your loan press ENTER:");
             Console.ReadLine();
 
